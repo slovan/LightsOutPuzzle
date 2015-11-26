671e6e8da47b5053c05ffc1a3d7f2cc8e1fc3d36
Volodymyr Ponomarenko <volod.ponomar@gmail.com>
2015-11-26 23:28:47 +0100

    Add comments to QuietPatterns class

--- LightsOutPuzzle/src/com/lightsout/twoStates/QuietPatterns.java
+++ LightsOutPuzzle/src/com/lightsout/twoStates/QuietPatterns.java
@@ -1,20 +1,29 @@
 package com.lightsout.twoStates;
 
+// Quiet patterns - sequences of button presses that will leave the lights unchanged
 public class QuietPatterns {
-	private int[][] matrix;
+	private int[][] matrix; // matrix of lights out matrix, to be changed with
+							// Gauss-Jordan elimination
 	private int[][] quietPatterns;
-	private int size = 5;
-	private int nullity;
-	
-	public QuietPatterns(){
-		GaussJordanElim gje = new GaussJordanElim(new LightsOutMatrix(size).getLightsOutMatrix());
+	private int size; // size of game
+	private int nullity; // the dimension of the null space of the lights out
+							// matrix
+
+	public QuietPatterns() {
+		this.size = InitialConfig.getSize();
+
+		LightsOutMatrix lom = new LightsOutMatrix();
+		GaussJordanElim gje = new GaussJordanElim(lom.getLightsOutMatrix());
 		this.matrix = gje.getMatrix();
-		this.nullity = size*size - gje.getRank();
-		this.quietPatterns = new int[(int)Math.pow(2, nullity)][size*size];
+
+		this.nullity = size * size - gje.getRank();
+
+		this.quietPatterns = new int[(int) Math.pow(2, nullity)][size * size];
 		findQuietPatterns();
 	}
-		
-	public void findQuietPatterns(){
+
+	public void findQuietPatterns() {
+		// flag to mark position of dependent variables
 		boolean[] pFlags = new boolean[this.matrix.length];
 		for (int i = 0, j = 0; j < this.matrix[i].length; j++) {
 			if (this.matrix[i][j] != 0) {
@@ -24,15 +33,19 @@ public class QuietPatterns {
 				pFlags[j] = false;
 			}
 		}
-		int[][] nullMatrix = new int[(int)Math.pow(2, nullity)][nullity];
+
+		// find all combinations of dependent variables in Finite Field of size 2
+		// and write them to matrix nullMatrix
+		int[][] nullMatrix = new int[(int) Math.pow(2, nullity)][nullity];
 		for (int i = 0; i < nullMatrix.length; i++) {
 			String str = Integer.toBinaryString(i);
 			while (nullity > str.length())
 				str = "0" + str;
 			for (int j = 0; j < nullMatrix[i].length; j++) {
-				nullMatrix[i][j] = Integer.parseInt(str.substring(j, j+1));
+				nullMatrix[i][j] = Integer.parseInt(str.substring(j, j + 1));
 			}
 		}
+
 		// include nullMatrix in quietPatterns:
 		for (int i = 0; i < quietPatterns.length; i++) {
 			for (int j = 0, p = 0; j < quietPatterns[i].length; j++) {
@@ -41,19 +54,20 @@ public class QuietPatterns {
 					p++;
 				} else {
 					quietPatterns[i][j] = 1;
-				}			
+				}
 			}
 		}
-		// fulfill quietPatterns
+
+		// fulfill quietPatterns matrix
 		for (int i = 0; i < quietPatterns.length; i++) {
 			for (int j = 0; j < quietPatterns[i].length; j++) {
 				if (pFlags[j]) {
 					int sum = 0;
 					for (int k = j + 1; k < quietPatterns[i].length; k++) {
-						sum += quietPatterns[i][k]*matrix[j][k];
+						sum += quietPatterns[i][k] * matrix[j][k];
 					}
 					quietPatterns[i][j] = (2 - sum) % 2;
-				}			
+				}
 			}
 		}
 	}
@@ -89,6 +103,5 @@ public class QuietPatterns {
 	public void setNullity(int nullity) {
 		this.nullity = nullity;
 	}
-	
-	
+
 }
