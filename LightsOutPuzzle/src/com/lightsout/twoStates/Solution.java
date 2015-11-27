3c49d6fbe7ec55ea0a145abc690697da3c6663ed
Volodymyr Ponomarenko <volod.ponomar@gmail.com>
2015-11-27 01:07:24 +0100

    Fixes of Solution class

--- LightsOutPuzzle/src/com/lightsout/twoStates/Solution.java
+++ LightsOutPuzzle/src/com/lightsout/twoStates/Solution.java
@@ -1,44 +1,124 @@
 package com.lightsout.twoStates;
 
+// find all strategies of solutions if exists
+// determine optimal solution (the least number of steps)
 public class Solution {
-	private Boolean isSolution;
+	private Boolean isSolution; // indicate an existence of any solution
+	private int size; // size of the game
 	private int[][] gaussJordanMatrix;
-	private int size = 5;
-	
+	private int[][] solutions; // contain all solutions
+	private int[][] optimalSolution;
+
 	public Solution() {
-		GaussTwoStates geb = new GaussTwoStates(new LightsOutMatrix(size).getLightsOutMatrix());
+
+		new InitialConfig(); // must be run at the beginning of program to set
+								// values to its static attributes
+
+		this.size = InitialConfig.getSize(); // set size of game
+
+		GaussTwoStates geb = new GaussTwoStates(new LightsOutMatrix().getLightsOutMatrix());
+
 		GaussJordanElim gje = new GaussJordanElim(new AugmentedMatrix().getAugMatrix());
 		this.gaussJordanMatrix = gje.getMatrix();
+
 		if (geb.getRank() == gje.getRank())
 			this.isSolution = true;
 		else
 			this.isSolution = false;
+
+		findSolutions();
+		findOptimalSolution();
 	}
-	
+
 	public void findSolutions() {
 		if (isSolution) {
-			QuietPatterns qp = new QuietPatterns();
-			int quantSol = (int)Math.pow(2, qp.getNullity());
-			for (int i = 0; i < quantSol; i++) {
-				int[] sol = new int[size*size];
-				for (int j = 0; j < sol.length; j++) {
-					sol[j] = this.gaussJordanMatrix[j][this.gaussJordanMatrix[j].length - 1];
-					sol[j] = (sol[j] + qp.getQuietPatterns()[i][j])%2;
+			QuietPatterns qp = new QuietPatterns(); // generate quiet patterns
+													// for the size of game
+			this.solutions = qp.getQuietPatterns(); // set reference to quiet
+													// patterns matrix
+
+			// loop to find the solution matrix
+			for (int i = 0; i < solutions.length; i++) {
+				for (int j = 0; j < solutions[i].length; j++) {
+					solutions[i][j] = (gaussJordanMatrix[j][size * size] + solutions[i][j]) % 2;
 				}
-				System.out.println("Solution " + (i+1) + " is:");
-				printSolution(sol);
 			}
 		} else {
-			System.out.println("The solution of such combination does not exist!");
+			this.solutions = null;
 		}
 	}
-	
-	public void printSolution(int[] sol) {
-		for (int i = 0; i < this.size; i++) {
-			for (int k = 0; k < this.size; k++) {
-				System.out.printf("%1d ", sol[k + i*size]);
+
+	public void findOptimalSolution() {
+		if (isSolution) {
+			// initialization of optimal solution matrix
+			this.optimalSolution = new int[size][size];
+
+			int countMIN = size * size; // counter of steps in the optimal
+										// solution
+			int index = 0; // index of row of the solutions matrix with the
+							// optimal solution
+			for (int i = 0; i < solutions.length; i++) {
+				int count = 0; // counter of steps in the current solution
+				for (int j = 0; j < solutions[i].length; j++) {
+					if (solutions[i][j] == 1)
+						count++;
+				}
+				if (count < countMIN) {
+					countMIN = count;
+					index = i;
+				}
 			}
-			System.out.println();
+
+			// make square matrix from the row of the optimal solution
+			for (int i = 0; i < optimalSolution.length; i++) {
+				for (int j = 0; j < optimalSolution[i].length; j++) {
+					optimalSolution[i][j] = solutions[index][i * size + j];
+				}
+			}
+
+		} else {
+			this.optimalSolution = null;
 		}
 	}
+
+	public Boolean getIsSolution() {
+		return isSolution;
+	}
+
+	public void setIsSolution(Boolean isSolution) {
+		this.isSolution = isSolution;
+	}
+
+	public int getSize() {
+		return size;
+	}
+
+	public void setSize(int size) {
+		this.size = size;
+	}
+
+	public int[][] getGaussJordanMatrix() {
+		return gaussJordanMatrix;
+	}
+
+	public void setGaussJordanMatrix(int[][] gaussJordanMatrix) {
+		this.gaussJordanMatrix = gaussJordanMatrix;
+	}
+
+	public int[][] getSolutions() {
+		return solutions;
+	}
+
+	public void setSolutions(int[][] solutions) {
+		this.solutions = solutions;
+	}
+
+	public int[][] getOptimalSolution() {
+		return optimalSolution;
+	}
+
+	public void setOptimalSolution(int[][] optimalSolution) {
+		this.optimalSolution = optimalSolution;
+	}
+
 }
